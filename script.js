45e8e7501dce42fb17bc269c8b833ea2be951940
Caacaaaa <yuriskazsalsabilla@gmail.com>
2026-02-21 20:13:35 +0700

    final result

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-let tempC = 26
-let currentUnit ="C"
-let condition = "cloudy"
+const url = "https://api.open-meteo.com/v1/forecast?latitude=-5.43&longitude=105.26&current_weather=true&timezone=auto";
 
-function updateUI(){
-    let temp = tempC
-    if (currentUnit === "F") {
-        temp = (tempC * 9/5) + 32
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+
+    const tempC = data.current_weather.temperature;
+    const tempF = (tempC * 9/5) + 32;
+    const code = data.current_weather.weathercode;
+
+    document.getElementById("tempC").textContent = Math.round(tempC) + "°C";
+    document.getElementById("tempF").textContent = Math.round(tempF) + "°F";
+
+    let text = "it's cloudy today ☁️";
+    let image = "assets/cloudy (1).png";
+
+    if (code >= 61) {
+      text = "it's rainy today 🌧️";
+      image = "assets/rainy (1).png"; // pastikan file ini ada
     }
-    document.getElementById("tempValue").innerText = Math.round(temp)
 
-    const img = document.getElementById("bgImage")
-    const text = document.getElementById("WeatherText")
+    document.getElementById("weatherText").textContent = text;
+    document.getElementById("bgImage").src = image;
 
-    if(condition == "rainy") {
-        img.src = "assets/rainy (1).png"
-        text.innerText = "it's rainy today"
-    } else {
-        img.src = "assets/cloudy (1).png"
-        text.innerText = "it's rainy today"
-    }
-}
+  })
+  .catch(err => {
+    console.log("Error:", err);
+  });
 
-function setUnit(unit){
-    currentUnit = unit
-    updateUI()
-}
+  getWeather(); 
 
-function changeWeather() {
-    condition = condition === "cloudy" ? "rainy" : "cloudy"
-    updateUI()
-}
-
-updateUI()
+setInterval(() => {
+  getWeather();
+}, 60000);
